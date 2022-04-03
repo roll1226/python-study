940c41025b28e80c4ec786fa96414e3d9a1a261a
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-03 11:52:49 +0900

    feat: 違う端末の画像の類似度

--- opt/image.py
+++ opt/image.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import imagehash
 
-hash = imagehash.average_hash(Image.open('images/one.jpg'))
+# hash = imagehash.average_hash(Image.open('images/one.jpg'))
+hash = imagehash.average_hash(Image.open('images/desk-iphone.jpg'))
 print(hash)
 
-otherhash = imagehash.average_hash(Image.open('images/other.jpg'))
+# otherhash = imagehash.average_hash(Image.open('images/other.jpg'))
+otherhash = imagehash.average_hash(Image.open('images/desk-ipad.jpg'))
 print(otherhash)
 
 print(hash == otherhash)
