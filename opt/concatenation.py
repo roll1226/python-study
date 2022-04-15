51f6ad4ab3b0a47480bea8b1f656360fd23b4b80
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-15 22:59:41 +0900

    feat: concatenation & repeat

---
+++ opt/concatenation.py
@@ -0,0 +1,4 @@
+print("ABC" + "DEF" + "GHI")
+str1 = "Hello,"
+str2 = "Python"
+print(str1 + str2)
---
+++ opt/repeat.py
@@ -0,0 +1,7 @@
+from turtle import st
+
+
+print("ABC" * 4)
+str = "Hello !"
+n = 3
+print(str * n)
