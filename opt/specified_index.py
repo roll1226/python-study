b71d96aa8180f3f64d9f54d1b4325d742bc1b028
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-17 20:57:13 +0900

    feat: specified index

---
+++ opt/specified_index.py
@@ -0,0 +1,16 @@
+str1 = "Hello"
+print(str1[1])
+print(str1[-1])
+
+str2 = "東京都"
+print(str2[2])
+
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+
+print(str2[-3])
+print(str2[-2])
+print(str2[-1])
