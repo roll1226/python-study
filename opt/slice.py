edcf4010e51c872920100ffd85bc422081d3f292
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-20 22:50:09 +0900

    feat: slice

---
+++ opt/slice.py
@@ -0,0 +1,11 @@
+str = "Flower"
+print(str[1:4])
+print(str[2:6])
+print(str[:3])
+print(str[4:])
+print(str[:])
+
+str2 = "京都府京都市左京区吉田本町"
+print(str2[1:12])
+print(str2[1:12:2])
+print(str2[1:12:3])
