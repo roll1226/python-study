87e8cb767ab8ab2f2cf2c8f91302858d2eee1133
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-30 08:42:23 +0900

    feat: change string method

---
+++ opt/change_string_method.py
@@ -0,0 +1,29 @@
+print("Hello".lower())
+print("APPLE".lower())
+
+print("Ｈｅｌｌｏ".lower())
+print("ＡＰＰＬＥ".lower())
+
+print("Hello".upper())
+print("apple".upper())
+
+print("Ｈｅｌｌｏ".upper())
+print("ａｐｐｌｅ".upper())
+
+print("hello python".capitalize())
+print("MY FAVORITE THINGS".capitalize())
+
+print("ｈｅｌｌｏ ｐｙｔｈｏｎ".capitalize())
+print("ＭＹ ＦＡＶＯＲＩＴＥ ＴＨＩＮＧＳ".capitalize())
+
+print("hello python".title())
+print("MY FAVORITE THINGS".title())
+
+print("ｈｅｌｌｏ ｐｙｔｈｏｎ".title())
+print("ＭＹ ＦＡＶＯＲＩＴＥ ＴＨＩＮＧＳ".title())
+
+print("Hello".swapcase())
+print("apple".swapcase())
+
+print("Ｈｅｌｌｏ".swapcase())
+print("ａｐｐｌｅ".swapcase())
